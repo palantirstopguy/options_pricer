c4e7a5e53d5b382b10131eeb8c9aecc2f42efbe5
palantirstopguy <kocmateos@gmail.com>
2026-04-28 23:49:36 +0200

    Finished Monte Carlo file for open use

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,6 +1,20 @@
 import numpy as np
 
-def Monte_Carlo(S0,T,r,sigma,dt,K):
+"""
+    Parameter:
+        S0    : Starting price of the stock in USD
+        K     : Strike Price
+        r     : riskfree interest (e.g. 0.05 = 5%)
+        sigma : Volatility of the stock (e.g. 0.2 = 20%)
+        T     : Number of time steps (e.g. 252 trading days = one year)
+        dt    : size of a single time step in years (e.g. 1/252 for one trading day)
+
+    Returns : 
+        C     : fair price of the call option in USD    
+"""
+
+
+def Monte_Carlo(S0,K,r,sigma,T,dt):
     prices = np.zeros(T)
     prices[0] = S0
 
