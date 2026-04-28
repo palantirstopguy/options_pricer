8533830c79b09c8060fd9f8182e8f3462d862b38
palantirstopguy <kocmateos@gmail.com>
2026-04-28 23:50:09 +0200

    Finished the Black-Scholes file for open use

--- black_scholes.py
+++ black_scholes.py
@@ -1,7 +1,19 @@
 import numpy as np
 import scipy.stats as scs
 
-def Black_Scholes(S,K,r,T,sigma):
+"""
+    Parameter:
+        S     : current stock price in USD
+        K     : Strike Price
+        r     : riskfree interest (e.g. 0.05 = 5%)
+        sigma : Volatility of the stock (e.g. 0.2 = 20%)
+        T     : Time until maturity in years (e.g. 1 = one year)
+
+    Returns : 
+        C     : fair price of the call option in USD    
+"""
+
+def Black_Scholes(S,K,r,sigma,T):
     d1 = (np.log(S/K) + (r+(sigma**2/2))*T)/(sigma*np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
 
