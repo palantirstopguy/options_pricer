1f9208bfb16829f1f10919ca36a7fbea55ce2f4d
palantirstopguy <kocmateos@gmail.com>
2026-04-29 20:33:19 +0200

    added put options

--- black_scholes.py
+++ black_scholes.py
@@ -20,3 +20,9 @@ def Black_Scholes(S,K,r,sigma,T):
     C = S * scs.norm.cdf(d1) - K*np.exp(-r*T) * scs.norm.cdf(d2)
     return C
 
+def Black_Scholes_Put(S,K,r,sigma,T):
+    d1 = (np.log(S/K) + (r+(sigma**2/2))*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    C = K*np.exp(-r*T) * scs.norm.cdf(-d2) - S*scs.norm(-d1)
+    return C
