54c9cb6b9619a13104ed6afaa7d3c9839f080148
palantirstopguy <kocmateos@gmail.com>
2026-06-08 18:28:36 +0200

    Finished adding greeks

--- greeks.py
+++ greeks.py
@@ -3,17 +3,17 @@ import scipy.stats as scs
 from utils import calculate_d1_d2
 
 
-def delta_call(S,K,r,sigma,T):
+def calculate_delta_call(S,K,r,sigma,T):
     d1,d2 = calculate_d1_d2(S,K,r,sigma,T)
-    d_call = scs.norm.cdf(d1)
-    return d_call
+    delta_call = scs.norm.cdf(d1)
+    return delta_call
 
-def delta_put(S,K,r,sigma,T):
+def calculate_delta_put(S,K,r,sigma,T):
     d1,d2 = calculate_d1_d2(S,K,r,sigma,T)
-    d_put = scs.norm.cdf(d1) - 1
-    return d_put
+    delta_put = scs.norm.cdf(d1) - 1
+    return delta_put
 
-def gamma(S,K,r,sigma,T):
+def calculate_gamma(S,K,r,sigma,T):
     d1,d2 = calculate_d1_d2(S,K,r,sigma,T)
-    g = (scs.norm.pdf(d1)) / (S*sigma*np.sqrt(T))
-    return g
+    gamma = (scs.norm.pdf(d1)) / (S*sigma*np.sqrt(T))
+    return gamma
